535dd3c9e6284c88c7c932d6a6194ed127e170ea
Alexander Allen <Alexander Allen>
2017-12-03 17:19:56 -0800

    Add project files.

---
+++ selenium1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace selenium1
+{
+    class Tst
+    {
+        static void Main(string[] args)
+        {
+            TestChrome();
+        }
+
+        static void TestChrome()
+        {
+            IWebDriver drv = new ChromeDriver();
+            drv.Navigate().GoToUrl("http://google.com");
+            IWebElement query = drv.FindElement(By.Name("q"));
+            query.SendKeys("abc");
+            query.Submit();
+            drv.Quit();
+
+
+        }
+    }
+}
